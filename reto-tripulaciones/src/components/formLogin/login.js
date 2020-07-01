c6701dabe43122970c2b4a57da812533f432ce46
Ulises <dr.manzanas1@gmail.com>
2020-07-01 19:27:40 +0200

    login falta oauth

--- reto-tripulaciones/src/components/formLogin/login.js
+++ reto-tripulaciones/src/components/formLogin/login.js
@@ -1,9 +1,21 @@
-import React,{Component} from 'react'
-
-class login extends Component {
-    constructor(){
-        this.state = {
-            
-        }
+import React,{ Component } from 'react';
+import './login.css';
+class Login extends Component {
+    render(){
+        return(
+            <form method="POST">
+                <div>
+                <label for="usuario">Usuario:</label>
+                <input type="text" id="usuario" name="user_name" required />
+                </div>
+                <div>
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" name="password" required />
+                </div>
+                <button>Entrar</button>
+            </form>
+        )
     }
 }
+
+export default Login

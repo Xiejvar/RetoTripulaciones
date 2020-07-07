bc8a0d965ef7ba1f5076549febf1f004a644d259
Ulises <dr.manzanas1@gmail.com>
2020-07-07 20:50:02 +0200

    conect node,react

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import SignUp from './components/signUp/signUp'
+import Login from './components/Login/Login'
+import IniciadoSesion from './components/IniciadoSesion/IniciadoSesion'
+import {Switch,Route} from 'react-router-dom'
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state= {
+      testApi: ''
+    }
+  }
+  callAPI(){
+    fetch('http://localhost:9000/testApi')
+      .then(res => res.text())
+      .then(res => this.setState({
+        testApi: res
+      }))
+  }
+  componentDidMount(){
+    this.callAPI()
+  }
+  render(){
+      return (
+        <div className="App">
+          <p>{this.state.testApi}</p>
+          {/* <Switch>
+            <Route exact path="/" component={IniciadoSesion} />
+            <Route  path="/iniciarSesion" component={Login} />
+            <Route  path="/registrarSesion" component={SignUp} />
+          </Switch> */}
+        </div>
+      )
+  }
+}
+export default App;
